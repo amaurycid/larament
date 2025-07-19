84b006d2eed0fd91c8d0949e9e32a55e722cb0cc
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-19 16:53:13 +0200

    Add welcome command to post-create-project-cmd

---
+++ app/Console/Commands/Welcome.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+final class Welcome extends Command
+{
+    protected $signature = 'app:welcome';
+
+    protected $description = 'Scaffold a new Laravel SaaS project the Larament way.';
+
+    public function handle(): void
+    {
+        $orangeColor = "\x1b[33m";
+        $resetColor = "\x1b[0m";
+
+        $this->line($orangeColor.'██╗      █████╗ ██████╗   █████╗ ███╗   ███╗███████╗███╗   ███╗████████╗'.$resetColor);
+        $this->line($orangeColor.'██║     ██╔══██╗██╔══██╗ ██╔══██╗████╗ ████║██╔════╝████╗ ████║╚══██╔══╝'.$resetColor);
+        $this->line($orangeColor.'██║     ███████║██████╔╝ ███████║██╔████╔██║█████╗  ██╔████╔██║   ██║   '.$resetColor);
+        $this->line($orangeColor.'██║     ██╔══██║██╔══██╗ ██╔══██║██║╚██╔╝██║██╔══╝  ██║╚██╔╝██║   ██║   '.$resetColor);
+        $this->line($orangeColor.'███████╗██║  ██║██║  ██║ ██║  ██║██║ ╚═╝ ██║███████╗██║ ╚═╝ ██║   ██║   '.$resetColor);
+        $this->line($orangeColor.'╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═╝     ╚═╝   ╚═╝   '.$resetColor);
+    }
+}
