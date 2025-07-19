5598a2da551c989979cbe55c17244ceb64ef892b
CodeWithDennis <personal.elsinga@gmail.com>
2025-07-19 17:06:25 +0200

    Add cleanup script to remove specified files and directories

---
+++ cleanup.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+$filesToRemove = [
+    'README.md',
+    'resources/images/users-table.png',
+    'resources/images/tests.png',
+    'resources/images/login-page.png',
+    'resources/images',
+    __FILE__
+];
+
+foreach ($filesToRemove as $file) {
+    if (file_exists($file)) {
+        if (is_file($file)) {
+            unlink($file);
+        } elseif (is_dir($file)) {
+            removeDirectory($file);
+        }
+    }
+}
+
+file_put_contents('README.md', '');
+
+function removeDirectory(string $dir): bool
+{
+    if (!is_dir($dir)) {
+        return false;
+    }
+
+    $files = array_diff(scandir($dir), ['.', '..']);
+    
+    foreach ($files as $file) {
+        $path = $dir . DIRECTORY_SEPARATOR . $file;
+        
+        if (is_dir($path)) {
+            removeDirectory($path);
+        } else {
+            unlink($path);
+        }
+    }
+    
+    return rmdir($dir);
+} 
